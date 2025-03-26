20ab2b62851db7de236ef363f43a1e5ccbb35239
Ediongsenyene Joseph <ediongi6@hotmail.com>
2025-03-26 20:02:02 +0100

    chores: add linter

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    ignores: [
+      "build",
+      "coverage",
+      "node_modules",
+      ".angular",
+      ".output",
+      "dist",
+    ],
+  },
+  tseslint.configs.recommended,
+]);
