ee63e8bb97d697f264d05ce2db49a1c4332708ce
Ediongsenyene Joseph <ediongi6@hotmail.com>
2025-03-27 16:59:01 +0100

    chores: clean installations

--- src/app/shared/directives/filter-button/filter-button.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterButtonDirective } from './filter-button.directive';
-
-describe('FilterButtonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FilterButtonDirective();
-    expect(directive).toBeTruthy();
-  });
-});
