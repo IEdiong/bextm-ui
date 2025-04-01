f61754a8bb0bf1e41fc9e9067af8d957d4554461
Ediongsenyene Joseph <ediongi6@hotmail.com>
2025-04-01 10:55:06 +0100

    feat(animation): add animation for better UX

--- src/app/extension-list/extension-list.component.ts
+++ src/app/extension-list/extension-list.component.ts
@@ -1,15 +1,26 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { ExtensionStore } from '../store/extensions.store';
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'bem-extension-list',
   standalone: true,
   imports: [CardComponent],
   template: `
-    <ul class="extensions-container">
+    <ul
+      class="extensions-container"
+      [@listAnimation]="store.filteredExtensions().length"
+    >
       @for (extension of store.filteredExtensions(); track extension.name) {
-        <li>
+        <li [@itemAnimation]>
           <bem-card
             [logo]="extension.logo"
             [name]="extension.name"
@@ -22,6 +33,32 @@ import { ExtensionStore } from '../store/extensions.store';
       }
     </ul>
   `,
+  animations: [
+    trigger('listAnimation', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, scale: 0.5 }),
+            stagger(50, [
+              animate('300ms ease-out', style({ opacity: 1, scale: 1 })),
+            ]),
+          ],
+          { optional: true },
+        ),
+        query(
+          ':leave',
+          [animate('300ms ease-out', style({ opacity: 0, scale: 0.5 }))],
+          { optional: true },
+        ),
+      ]),
+    ]),
+    trigger('itemAnimation', [
+      transition(':leave', [
+        animate('200ms ease-out', style({ opacity: 0, scale: 0 })),
+      ]),
+    ]),
+  ],
   styles: `
     :host {
       display: block;
